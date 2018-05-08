54ea4a7c890764fe2569624db1cfb5ec7bb88fd2
Travis CI User <travis@example.org>
2018-05-08 18:00:07 +0000

    Documentation upload

---
+++ implementors/actix/stream/trait.StreamHandler.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["actix_redis"] = ["impl StreamHandler&lt;<a class=\"enum\" href=\"actix_redis/enum.RespValue.html\" title=\"enum actix_redis::RespValue\">RespValue</a>, <a class=\"enum\" href=\"actix_redis/enum.RespError.html\" title=\"enum actix_redis::RespError\">RespError</a>&gt; for <a class=\"struct\" href=\"actix_redis/struct.RedisActor.html\" title=\"struct actix_redis::RedisActor\">RedisActor</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
